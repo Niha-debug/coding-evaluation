0d835c66be0b70df740097cee741d0d17410553b
Niha-debug <n.godthi1@gmail.com>
2023-04-13 11:19:13 -0400

    Updated hire module

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -5,30 +5,57 @@ import java.util.Optional;
 public abstract class Organization {
 
 	private Position root;
-	
+	private int lastAssignedIdentifier;
+
 	public Organization() {
 		root = createOrganization();
+		lastAssignedIdentifier=0;
 	}
-	
+
 	protected abstract Position createOrganization();
-	
+
 	/**
 	 * hire the given person as an employee in the position that has that title
-	 * 
+	 *
 	 * @param person
 	 * @param title
 	 * @return the newly filled position or empty if no position has that title
 	 */
 	public Optional<Position> hire(Name person, String title) {
-		//your code here
+		return searchAndFillPosition(root, person, title);
+	}
+
+	private Optional<Position> searchAndFillPosition(Position pos, Name person, String title) {
+		// Check if the current position has the required title and is not filled
+		if (pos.getTitle().equals(title) && !pos.isFilled()) {
+			// Fill the position with the given employee
+			pos.setEmployee(Optional.of(new Employee(getNextIdentifier(), person)));
+			return Optional.of(pos);
+		}
+
+		// Recursively search for the required title in direct reports
+		for (Position directReport : pos.getDirectReports()) {
+			Optional<Position> filledPosition = searchAndFillPosition(directReport, person, title);
+			if (filledPosition.isPresent()) {
+				return filledPosition;
+			}
+		}
+
+		// Return empty optional if the required title is not found
 		return Optional.empty();
 	}
+
+	private int getNextIdentifier() {
+		// This is a simplified implementation to assign unique identifiers
+		return ++lastAssignedIdentifier;
+	}
+
 
 	@Override
 	public String toString() {
 		return printOrganization(root, "");
 	}
-	
+
 	private String printOrganization(Position pos, String prefix) {
 		StringBuffer sb = new StringBuffer(prefix + "+-" + pos.toString() + "\n");
 		for(Position p : pos.getDirectReports()) {
