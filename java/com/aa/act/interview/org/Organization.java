97f028df3f9c3db2d4c438374f7ec76b2441edf4
Niha-debug <121262253+Niha-debug@users.noreply.github.com>
2023-06-21 13:15:33 -0400

    updated

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -5,34 +5,50 @@ import java.util.Optional;
 public abstract class Organization {
 
     private Position root;
-    
+
+    private static int id;
+
     public Organization() {
         root = createOrganization();
     }
-    
+
     protected abstract Position createOrganization();
-    
+
     /**
      * hire the given person as an employee in the position that has that title
-     * 
+     *
      * @param person
      * @param title
      * @return the newly filled position or empty if no position has that title
      */
     public Optional<Position> hire(Name person, String title) {
-        //your code here
-        return Optional.empty();
+        addEmployee(root, person, title);
+        plugDetails(root, person, title);
+        return Optional.of(root);
+    }
+
+    private void addEmployee(Position position, Name person, String title) {
+        if (position.getTitle().equals(title)) {
+            position.setEmployee(Optional.of(new Employee(++id, new Name(person.getFirst(), person.getLast()))));
+        }
+
+    }
+    private void plugDetails(Position pos, Name person, String title) {
+        for (Position p : pos.getDirectReports()) {
+            addEmployee(p, person, title);
+            plugDetails(p, person, title);
+        }
     }
 
     @Override
     public String toString() {
         return printOrganization(root, "");
     }
-    
+
     private String printOrganization(Position pos, String prefix) {
         StringBuffer sb = new StringBuffer(prefix + "+-" + pos.toString() + "\n");
-        for(Position p : pos.getDirectReports()) {
-            sb.append(printOrganization(p, prefix + "  "));
+        for (Position p : pos.getDirectReports()) {
+            sb.append(printOrganization(p, prefix + "\t"));
         }
         return sb.toString();
     }
